6adf2c0fd6e0de09e06d1d79f8a8520fd8ee5890
Patrick Cheung <patrickcheungdev@gmail.com>
2015-12-26 22:13:41 -0800

    updated with simple test scenario

--- src/SudokuProgram.java
+++ src/SudokuProgram.java
@@ -1,5 +1,26 @@
 /**
+ * Main program that runs the input, output, and sudoku solving
+ * @author Patrick Cheung
+ * @version 1.0
  * Created by patch on 2015-12-26.
  */
 public class SudokuProgram {
-}
+    public static void main(String[] args){
+        //0 means empty cell
+        int[][] grid = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                        {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                        {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                        {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                        {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                        {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                        {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+
+        SudokuSolver solve = new SudokuSolver();
+        if(solve.solveSudoku(grid))
+            solve.printSolution(grid);
+        else
+            System.out.println("No Solution Exists.");
+    }// end main
+}// end SudokuProgram
