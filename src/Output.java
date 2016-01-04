79d7bfbf67856411f3aa622c35c77cdbaac06595
Patrick Cheung <patrickcheungdev@gmail.com>
2016-01-03 21:40:05 -0800

    updated output

--- src/Output.java
+++ src/Output.java
@@ -1,9 +1,14 @@
 /**
- *
+ * Outputs puzzles into the text file "solutions.txt"
  * @author Patrick Cheung
  * @version 1.0
  * Created by patch on 2015-12-27.
  */
-public class Output {
+import java.util.ArrayList;
 
+public class Output {
+    //constructor
+    public Output(ArrayList<int[][]> sol){
+
+    }
 }// end Output
